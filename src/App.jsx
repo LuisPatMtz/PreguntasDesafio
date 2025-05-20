490474f448cb9db2d1f45090993f9ff63bb40dc0
LuisPatMtz <luispatmartinez@gmail.com>
2025-05-20 00:01:51 -0600

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Página inicial */}
         <Route path="/" element={<LoginRegistro />} />
 
-        {/* Acciones del estudiante (sin matrícula en URL) */}
         <Route path="/acciones-estudiante" element={<AccionesEstudiante />} />
 
-        {/* Panel del docente */}
         <Route path="/AdminPage" element={<AdminPage />} />
 
-        {/* Panel del estudiante (sin matrícula en URL) */}
         <Route path="/panel-estudiante" element={<StudentPanel />} />
 
-        {/* Agregar preguntas: sólo materia en la ruta */}
         <Route path="/agregar-pregunta/:materia" element={<AgregarPregunta />} />
       </Routes>
     </Router>
