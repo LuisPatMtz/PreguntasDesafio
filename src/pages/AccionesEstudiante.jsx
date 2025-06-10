3764d48b9ff4aaf27707f25cba031215b390af4b
LuisPatMtz <luispatmartinez@gmail.com>
2025-06-09 20:19:47 -0600

    xd

--- src/pages/AccionesEstudiante.jsx
+++ src/pages/AccionesEstudiante.jsx
@@ -46,7 +46,7 @@ const AccionesEstudiante = () => {
 
   const confirmarAsistencia = async () => {
     try {
-      const response = await fetch('https://v62mxrdy3g.execute-api.us-east-1.amazonaws.com/prod/confirmarAsistenciaRDS', {
+      const response = await fetch('aunnohttps://v62mxrdy3g.execute-api.us-east-1.amazonaws.com/prod/confirmarAsistenciaRDS', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ matricula })
